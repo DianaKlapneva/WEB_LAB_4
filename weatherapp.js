1aaf719fa7a731d69caf1e662c029c7461b76a6a
Клапнева Диана Дмитриевна <klapneva.diana@yandex.ru>
2026-01-10 19:09:27 +0300

    другой вариант!! удаляем кеши

--- weatherapp.js
+++ weatherapp.js
@@ -1,42 +1,52 @@
 window.onload = async function() {
     try {
+        console.log('старт');
         
-        //убираем кеши чтобы каждый раз забрасывать
+        localStorage.removeItem('lastGeolocation');
+        
         const position = await new Promise((resolve, reject) => {
+            let timeoutId;
+            
+            const success = (pos) => {
+                clearTimeout(timeoutId);
+                resolve(pos);
+            };
+            
+            const error = (err) => {
+                clearTimeout(timeoutId);
+                reject(err);
+            };
+            
             navigator.geolocation.getCurrentPosition(
-                (pos) => {
-                    console.log('геолокация напрямую');
-                    resolve(pos);
-                },
-                reject,
+                success,
+                error,
                 {
-                    enableHighAccuracy: false,
-                    timeout: 15000,
-                    maximumAge: 0, 
+                    enableHighAccuracy: true,
+                    timeout: 45000,
+                    maximumAge: 0,
                 }
             );
+            
+            //еще время
+            timeoutId = setTimeout(() => {
+                reject(new Error('+60секунд'));
+            }, 60000);
         });
         
         const params = {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
             accuracy: position.coords.accuracy,
-            timestamp: new Date(position.timestamp).toLocaleTimeString(),
+            timestamp: new Date().toLocaleTimeString()
         };
         
-        console.log('геолокация:', params);
-        
+        console.log('получена геолокация');
         localStorage.setItem('lastGeolocation', JSON.stringify(params));
         
         return params;
         
     } catch (error) {
-        console.error('Ошибка:', error.code, error.message);
-
-        const lastGeo = localStorage.getItem('lastGeolocation');
-        if (lastGeo) {
-            console.log('Используем сохраненную геолокацию из прошлой сессии:', JSON.parse(lastGeo));
-        }
+        console.error('не получена геолокация', error.message);
         
         return null;
     }
